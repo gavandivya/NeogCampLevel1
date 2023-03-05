f14dc86dbaf980d4c4e2efe6918db77d126a778d
Divya R. Gavandi <divya190399@gmail.com>
2023-03-05 23:22:01 +0530

    feat:FPQuestionSet-1

--- FP-QuestionSet1/FPQuestionSet1.js
+++ FP-QuestionSet1/FPQuestionSet1.js
@@ -6,152 +6,137 @@
 3. Do NOT use for-loops.
  */
 
-
 //1. Write an ES6 function that takes an array of numbers and returns an array with each number incremented by 3 using the map method.
 
-```jsx
-    // Your ES6 function here
-    
-    const numbers = [1, 2, 3, 4, 5];
-    console.log(incrementNumbers(numbers));
-    // Output: [4, 5, 6, 7, 8]
-    ```
+const numbers = [1, 2, 3, 4, 5];
+const incrementNumbers = () => numbers.map((ele) => ele + 3);
+console.log(incrementNumbers(numbers));
+// Output: [4, 5, 6, 7, 8]
 
-    //2. Write an ES6 function that takes an array of strings and returns an array with all the strings capitalised using the map method.
+//2. Write an ES6 function that takes an array of strings and returns an array with all the strings capitalised using the map method.
 
-    ```jsx
-    // Your ES6 function here
-    
-    const myArray = ['apple', 'banana', 'cherry'];
-    console.log(capitalizeArray(myArray));
-    // Output: ["APPLE", "BANANA", "CHERRY"]
-    ```
+const myArray = ['apple', 'banana', 'cherry'];
+const capitalizeArray = () => {
+  return myArray.map((ele) => ele.toUpperCase());
+}
+console.log(capitalizeArray(myArray));
+// Output: ["APPLE", "BANANA", "CHERRY"]
 
-    //3. Write an ES6 function that takes an array of objects representing people and returns an array of their ages using the map method.
+//3. Write an ES6 function that takes an array of objects representing people and returns an array of their ages using the map method.
 
-    ```jsx
-    // Your ES6 function here
-    
-    const people = [
-      { name: 'Ankit', age: 25 },
-      { name: 'Vinit', age: 24 },
-      { name: 'Shashi', age: 29 }
-    ];
-    const ages = getAges(people);
-    console.log(ages); // Output: [25, 24, 29]
-    ```
+const people = [
+  { name: 'Ankit', age: 25 },
+  { name: 'Vinit', age: 24 },
+  { name: 'Shashi', age: 29 }
+];
 
-    //4. Write an ES6 function that takes an array of objects representing products and returns an array with the product names in all lower case letters using the map method.
+const getAges = (arr) => arr.map((ele) => ele.age);
 
-    ```jsx
-    // Your ES6 function here
-    
-    const products = [
-      { name: 'Lip Balm', stock: 100 },
-      { name: 'PERFUME', stock: 400 },
-      { name: 'Socks', stock: 200 }
-    ];
-    console.log(getProductNamesInLowerCase(products));
-    // Output: ["lip balm", "perfume", "socks"]
-    ```
+const ages = getAges(people);
+console.log(ages); // Output: [25, 24, 29]
 
-    //5. Write an ES6 function that takes an array of objects representing cities and returns an array of their names using the map method.
+//4. Write an ES6 function that takes an array of objects representing products and returns an array with the product names in all lower case letters using the map method.
 
-    ```jsx
-    // Your ES6 function here
-    
-    const cities = [
-      { name: 'New York', population: 8538000 },
-      { name: 'Los Angeles', population: 3976000 },
-      { name: 'Bangalore', population: 13608000 }
-    ];
-    
-    console.log(getCityNames(cities)); 
-    // Output: ["New York", "Los Angeles", "Bangalore"]
-    ```
+const products = [
+  { name: 'Lip Balm', stock: 100 },
+  { name: 'PERFUME', stock: 400 },
+  { name: 'Socks', stock: 200 }
+];
+
+const getProductNamesInLowerCase = (arr) => arr.map((ele) => ele.name.toLowerCase());
+console.log(getProductNamesInLowerCase(products));
+// Output: ["lip balm", "perfume", "socks"]
+
+//5. Write an ES6 function that takes an array of objects representing cities and returns an array of their names using the map method.
+
+const cities = [
+  { name: 'New York', population: 8538000 },
+  { name: 'Los Angeles', population: 3976000 },
+  { name: 'Bangalore', population: 13608000 }
+];
+
+const getCityNames = (arr) => arr.map((ele) => ele.name)
+console.log(getCityNames(cities));
+// Output: ["New York", "Los Angeles", "Bangalore"]
 
 
-    //6. Write an ES6 function that takes an array of strings and returns an array with only the strings that have a length greater than 5.
+//6. Write an ES6 function that takes an array of strings and returns an array with only the strings that have a length greater than 5.
 
-    ```jsx
-    // Your ES6 function here
-    
-    const fruits = ['apple', 'banana', 'cherry', 'watermelon', 'pineapple'];
-    console.log(filterLongStrings(fruits)); 
-    // Output: ["banana", "cherry", "watermelon", "pineapple"]
-    ```
+const fruits = ['apple', 'banana', 'cherry', 'watermelon', 'pineapple'];
+const filterLongStrings = (arr) => arr.filter((ele) => ele.length > 5)
+console.log(filterLongStrings(fruits));
+// Output: ["banana", "cherry", "watermelon", "pineapple"]
 
-    //7. Write an ES6 function that takes an array of numbers and returns an array with only the numbers that are divisible by 3 and also by 5.
+//7. Write an ES6 function that takes an array of numbers and returns an array with only the numbers that are divisible by 3 and also by 5.
 
-    ```jsx
-    // Your ES6 code here
-    
-    const numbers = [1, 2, 30, 4, 15];
-    console.log(isDivisibleBy3and5(numbers));
-    // Output: [30, 15]
-    ```
+const numbers1 = [1, 2, 30, 4, 15];
+const isDivisibleBy3and5 = arr => arr.filter((ele) => ele % 3 == 0 && ele % 5 == 0)
+console.log(isDivisibleBy3and5(numbers1));
+// Output: [30, 15]
 
-    //8. Write an ES6 function that takes an array of objects with the properties name and age, and returns an array with only the objects that have an age greater than 30.
+//8. Write an ES6 function that takes an array of objects with the properties name and age, and returns an array with only the objects that have an age greater than 30.
 
-    ```jsx
-    // Your ES6 code here
-    
-    const people = [
-      { name: 'Rahul', age: 25 },
-      { name: 'Raj', age: 35 },
-      { name: 'Vijay', age: 45 }
-    ];
-    
-    console.log(filterByAge(people)); 
-    // Output: [{ name: 'Raj', age: 35 }, { name: 'Vijay', age: 45 }]
-    ```
+const people1 = [
+  { name: 'Rahul', age: 25 },
+  { name: 'Raj', age: 35 },
+  { name: 'Vijay', age: 45 }
+];
 
-    //9. Write an ES6 function that takes an array of objects with the properties name and score, and returns an array with only the objects that have a score greater than 80.
+const filterByAge = (arr) => arr.filter((ele) => ele.age > 30)
 
-    ```jsx
-    // Your ES6 code here
-    
-    const students = [
-      { name: 'Aditya', score: 85 },
-      { name: 'Bob', score: 75 },
-      { name: 'Charlie', score: 90 }
-    ];
-    
-    console.log(filterByScore(students)); 
-    // Output: [{ name: 'Aditya', score: 85 }, { name: 'Charlie', score: 90 }]
-    ```
+console.log(filterByAge(people1));
+// Output: [{ name: 'Raj', age: 35 }, { name: 'Vijay', age: 45 }]
 
-    ///10. Write an ES6 function that takes an array of objects with the properties name, age and city, and returns an array with only the objects that have a city property of "Indore" and age greater than 70.
+//9. Write an ES6 function that takes an array of objects with the properties name and score, and returns an array with only the objects that have a score greater than 80.
 
-    ```jsx
-    // Your ES6 code here
-    
-    const people = [
-      { name: 'Ridhima', age: 75, city: 'Indore' },
-      { name: 'Akshay', age: 60, city: 'Delhi' },
-      { name: 'Udit', age: 80, city: 'Indore' },
-    	{ name: 'Venki', age: 80, city: 'Bangalore' }
-    ];
-    const filteredPeople = filterByCityAndAge(people);
-    console.log(filteredPeople); 
-    // Output: [{ name: 'Ridhima', age: 75, city: 'Indore' }, { name: 'Udit', age: 80, city: 'Indore' }]
-    ```
+const students = [
+  { name: 'Aditya', score: 85 },
+  { name: 'Bob', score: 75 },
+  { name: 'Charlie', score: 90 }
+];
 
-    //11. Write an ES6 function that takes an array of objects containing book information(title, author, genre) and returns an array with only the books of "Science Fiction".
+const filterByScore = (arr) => arr.filter((ele) => ele.score > 80)
 
-    ```jsx
-    // Your ES6 code here
-    
-    const books = [
-      {title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", genre: "Science Fiction"},
-      {title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Fiction"},
-      {title: "1984", author: "George Orwell", genre: "Science Fiction"},
-      {title: "Pride and Prejudice", author: "Jane Austen", genre: "Romance"}
-    ];
-    
-    const filteredBooks = filterByGenre(books, "Science Fiction");
-    console.log(filteredBooks);
-    // Output: [{title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", genre: "Science Fiction"}, {title: "1984", author: "George Orwell", genre: "Science Fiction"}]
-    ```
+console.log(filterByScore(students));
+// Output: [{ name: 'Aditya', score: 85 }, { name: 'Charlie', score: 90 }]
+
+///10. Write an ES6 function that takes an array of objects with the properties name, age and city, and returns an array with only the objects that have a city property of "Indore" and age greater than 70.
+
+const people3 = [
+  { name: 'Ridhima', age: 75, city: 'Indore' },
+  { name: 'Akshay', age: 60, city: 'Delhi' },
+  { name: 'Udit', age: 80, city: 'Indore' },
+  { name: 'Venki', age: 80, city: 'Bangalore' }
+];
+
+const filterByCityAndAge = (arr) => arr.filter((ele) => ele.city === "Indore" && ele.age > 70)
+const filteredPeople = filterByCityAndAge(people3);
+console.log(filteredPeople);
+// Output: [{ name: 'Ridhima', age: 75, city: 'Indore' }, { name: 'Udit', age: 80, city: 'Indore' }]
+
+//11. Write an ES6 function that takes an array of objects containing book information(title, author, genre) and returns an array with only the books of "Science Fiction".
+
+const books = [
+  { title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", genre: "Science Fiction" },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Fiction" },
+  { title: "1984", author: "George Orwell", genre: "Science Fiction" },
+  { title: "Pride and Prejudice", author: "Jane Austen", genre: "Romance" }
+];
+
+const filterByGenre = (arr, genre) => arr.filter((ele) => ele.genre === genre)
+
+const filteredBooks = filterByGenre(books, "Science Fiction");
+console.log(filteredBooks);
+// Output: [{title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", genre: "Science Fiction"}, {title: "1984", author: "George Orwell", genre: "Science Fiction"}]
 
 //12. Write an ES6 function that takes an array of objects containing employee information(name, age, salary) and returns an array with only the employees who earn a salary above 22000 and age is above 25.
+
+const employee = [
+  { name: 'Ridhima', age: 75, salary: 22000 },
+  { name: 'Akshay', age: 60, salary: 10000 },
+  { name: 'Udit', age: 25, salary: 50000 },
+  { name: 'Venki', age: 56, salary: 67000 }
+];
+
+const filterEmp = (arr) => arr.filter((ele) => ele.age > 25 && ele.salary > 22000)
+console.log(filterEmp(employee));
